3a337691e3c58689c5110c5e658661eb638ea442
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 19:35:01 +0800

    added github link to about me

--- react/src/components/About/About.jsx
+++ react/src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
     const { t } = useTranslation();
 
     return (
-        <>
+        <div className="about-me">
             <h1 className="title">{t('aboutmePage.aboutmeTitle')}</h1>
             <div className="about-me-content">
                 <p className="self-intro">
@@ -15,6 +15,7 @@ export default function About() {
                 </p>
                 <div className="my-img" style={{ backgroundImage: `url(${me})` }}></div>
             </div>
-        </>
+                <a className="github-link" href="https://github.com/Skylar2021" target="_blank" rel="noreferrer noopener">GitHub</a>
+        </div>
     )
 }
