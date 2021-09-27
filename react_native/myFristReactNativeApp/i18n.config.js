b986c8b291deaf2f9df4a35fceec6f3c1e1dc512
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 22:25:59 +0800

    added text of about page

--- react_native/myFristReactNativeApp/i18n.config.js
+++ react_native/myFristReactNativeApp/i18n.config.js
@@ -5,6 +5,7 @@ const translationEn = {
     tab: {
         login: 'Log In',
         home: 'Home',
+        about: 'About',
         gallery: 'Gallery',
         setting: 'Setting',
     },
@@ -15,11 +16,11 @@ const translationEn = {
     },
     welcomePage: {
         welcome: 'Welcome!',
-        selfIntro: 'Hello {{guestName}} I am Skylar. This is my first mobile application. I built this website with React Native.',
-        next:'Next',
+        selfIntro: 'Hello {{guestName}}~ I am Skylar. This is my first mobile application. I built this website with React Native.',
+        next: 'Next',
     },
-    galleryPage:{
-        text:'Find out more on my instagram',
+    galleryPage: {
+        text: 'Find out more on my instagram',
     },
     settingPage: {
         settingTitle: 'Setting',
@@ -27,11 +28,8 @@ const translationEn = {
         text: 'Are you sure to log out??',
     },
     aboutmePage: {
-        aboutmeTitle: 'About Me',
-        text: `A kind and responsible person who seeks an entry level front-end developer
-        position where I can apply my knowledge in frontend web development and
-        bring improvement to human life with innovation technology. I like taking photos(film), RPG game,
-        coding, reading, hiking, eating, listening to music, writing diary, travelling, movies...`
+        title: 'I am...',
+        text: `A kind and responsible person who seeks an entry level front-end developer position where I can apply my knowledge in frontend web development and bring improvement to human life with innovation technology. I like taking photos(film), RPG game, coding, reading, hiking, eating, listening to music, writing diary, travelling, movies...`
     },
     engButton: 'English',
     canButton: 'Cantonese',
@@ -42,6 +40,7 @@ const translationCan = {
         login: '登入',
         home: '主頁',
         gallery: '相片',
+        about: '關於',
         setting: '設定',
     },
     loginPage: {
@@ -52,11 +51,11 @@ const translationCan = {
     welcomePage: {
         welcome: '歡迎你！',
         selfIntro: '你好 {{guestName}}~ 我係Skylar，呢個手機應用程式係用React Navtive整，係我第一個嘅手機應用程式。',
-        next:'進入主畫面',
-    
+        next: '進入主畫面',
+
     },
-    galleryPage:{
-        text:'按此繼續欣賞我嘅創作',
+    galleryPage: {
+        text: '按此繼續欣賞我嘅創作',
     },
     settingPage: {
         logoutTitle: '設定',
@@ -64,7 +63,7 @@ const translationCan = {
         text: '真係走啦?',
     },
     aboutmePage: {
-        aboutmeTitle: '關於我',
+        title: '',
         text: `我係個友善有責任心嘅人，搵緊初級前端開發人員位置。我有一個抱負，希望我嘅工作能夠我嘅技能，成為推進社會積極進步嘅一個助力。
         我鍾意影菲林相、打(無聊)機、打code、睇書、行山、食野、聽歌、寫日記、去旅行、睇電影...等等。`,
 
