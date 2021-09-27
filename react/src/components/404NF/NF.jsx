f1ea731fd46332c5104cfdc2b4759a24664b19a2
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 15:26:26 +0800

    add link and text to error page

--- react/src/components/404NF/NF.jsx
+++ react/src/components/404NF/NF.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
-export default function NF() {
+export default function NF({isLogin}) {
+    const {t} = useTranslation()
     return (
-        <h1>404: Not Found</h1>
+        <div className="error-page">
+        <h1 className="error-page">404: Not Found</h1>
+        <hr/>
+        {isLogin ? <Link to="/home"><h4 className="error-link">{t('errorPage.textForLogined')}</h4></Link>:<Link to="/"><h4 className="error-link">{t('errorPage.textForLogin')}</h4></Link>}
+        </div>
+
     )
 }
