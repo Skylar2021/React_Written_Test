4526311e5257b0ed0acb992bcf01388434550027
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 19:37:49 +0800

    added gallery link

--- react/src/components/NavLink/NavLink.jsx
+++ react/src/components/NavLink/NavLink.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-export default function NavLink() {
+export default function NavLink({toggleNav}) {
     const { t } = useTranslation()
 
     return (
         <div className="nav-items">
 
-            <Link className="nav-item" to="/home">
+            <Link className="nav-item" to="/home" onClick={()=>toggleNav()}>
                 {t('link.home')}
             </Link>
-            <Link className="nav-item" to="/about-me">
+            <Link className="nav-item" to="/about-me" onClick={()=>toggleNav()}>
                 {t('link.aboutMe')}
             </Link>
-            <Link className="nav-item" to="/gallery">
+            <Link className="nav-item" to="/gallery" onClick={()=>toggleNav()}>
                 {t('link.gallery')}
             </Link>
-            <Link className="nav-item" to="/setting">
+            <Link className="nav-item" to="/setting" onClick={()=>toggleNav()}>
                 {t('link.setting')}
             </Link>
 
