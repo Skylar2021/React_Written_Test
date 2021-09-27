ed3e065876c269e1db7efc1250d0b5a9ffcb3109
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 15:28:21 +0800

    del redundant code

--- react/src/components/Welcome/Welcome.jsx
+++ react/src/components/Welcome/Welcome.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next';
-import LngChangeButton from '../LngChangeButton/LngChangeButton'
-import Navbar from '../Navbar/Navbar';
+import { Trans, useTranslation } from 'react-i18next';
 
-
-
-
-export default function Welcome({guestName,handleLngChange,isLogin}) {
-    const {t} = useTranslation();
+export default function Welcome({ guestName }) {
+    const { t } = useTranslation();
 
     return (
         <div>
-            {/* <Navbar isLogin={isLogin} handleLngChange={handleLngChange} /> */}
-
             <h1 className="title">{t("welcomePage.welcome")}</h1>
-            <p className="text-center">{t('welcomePage.selfIntro', {guestName} )}</p>
+            <p className="text-center">{t('welcomePage.greeting')} <i>{t('welcomePage.guestname', { guestName })} </i>{t('welcomePage.selfIntro')}</p>
         </div>
     )
 }
