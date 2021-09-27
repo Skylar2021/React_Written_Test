6ed4b8d809560f4d00993612f707ec6bb2f8d0b3
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-27 20:55:55 +0800

    added icons to navbar and edited css

--- react/src/components/NavLink/NavLink.jsx
+++ react/src/components/NavLink/NavLink.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-export default function NavLink({toggleNav}) {
+export default function NavLink({ toggleNav }) {
     const { t } = useTranslation()
 
     return (
         <div className="nav-items">
 
-            <Link className="nav-item" to="/home" onClick={()=>toggleNav()}>
+            <Link className="nav-item" to="/home" onClick={() => toggleNav()}>
+                <i className="fas fa-home nav-menu-icon"></i>
                 {t('link.home')}
             </Link>
-            <Link className="nav-item" to="/about-me" onClick={()=>toggleNav()}>
+            <Link className="nav-item" to="/about-me" onClick={() => toggleNav()}>
+                <i className="fas fa-smile nav-menu-icon"></i>
                 {t('link.aboutMe')}
             </Link>
-            <Link className="nav-item" to="/gallery" onClick={()=>toggleNav()}>
+            <Link className="nav-item" to="/gallery" onClick={() => toggleNav()}>
+                <i className="fas fa-film nav-menu-icon"></i>
                 {t('link.gallery')}
             </Link>
-            <Link className="nav-item" to="/setting" onClick={()=>toggleNav()}>
+            <Link className="nav-item" to="/setting" onClick={() => toggleNav()}>
+                <i className="fas fa-cog nav-menu-icon"></i>
                 {t('link.setting')}
             </Link>
 
