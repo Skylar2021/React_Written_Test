793cdf0c361f22601101374ba6c9692e05ab8e65
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-28 20:55:12 +0800

    equalize button, added title and emoji

--- react_native/myFristReactNativeApp/app/Welcome.js
+++ react_native/myFristReactNativeApp/app/Welcome.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { StyleSheet, ImageBackground, Text, Pressable } from 'react-native'
+import { StyleSheet, ImageBackground, Text, TouchableHighlight, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
-const Welcome = ({ handleIsAble, text, homeBtnText }) => {
+const Welcome = ({ handleIsAble, title, text, homeBtnText }) => {
     return (
-        <ImageBackground source={require('../assets/sea.jpg')} resizeMode="cover" style={styles.bgimage}>
-            <Text style={styles.welcomeText}>
-                {text}
-            </Text>
-            <Pressable style={styles.btnStyle} onPress={() => handleIsAble(true)}>
+        <ImageBackground imageStyle={{opacity:0.7}} source={require('../assets/sea.jpg')} resizeMode="cover" style={styles.bgimage}>
+            <View style={styles.textArea}>
+                <Text style={styles.welcomeTitle}>
+                    {title}
+                </Text>
+                <Text style={styles.welcomeText}>
+                    {text}<Ionicons name="md-happy-outline" size={24} color="#0040DF" />
+                </Text>
+            </View>
+            <TouchableHighlight underlayColor='#002782' style={styles.btnStyle} onPress={() => handleIsAble(true)}>
                 <Text style={styles.btnText}>{homeBtnText}</Text>
-            </Pressable>
+            </TouchableHighlight>
         </ImageBackground>
     )
 }
@@ -27,20 +33,30 @@ const styles = StyleSheet.create({
     },
     btnText: {
         color: '#fff',
-        fontWeight: '500',
+        fontWeight: '600',
+		fontSize:16,
     },
     bgimage: {
         flex: 1,
         justifyContent: 'space-evenly',
         alignItems: 'center',
-        padding: 10,
+        padding: 20,
+    },
+    textArea: {
+        backgroundColor: '#FFFFFF',
+        textAlign: 'center',
+        borderRadius: 10,
+        padding: 15,
+    },
+    welcomeTitle: {
+        fontSize: 25,
+        fontWeight: '600',
+        textAlign: 'center',
     },
     welcomeText: {
         textAlign: 'center',
-        fontSize: 18,
-        backgroundColor: '#FFFFFF',
+        fontSize: 17,
         padding: 5,
-        borderRadius: 10,
     },
 })
 export default Welcome
