63d865ba3a1b33390e94c8d0e9bd12982765893b
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-28 17:49:07 +0800

    edited indentation

--- react/src/components/Skill/Skill.js
+++ react/src/components/Skill/Skill.js
@@ -7,27 +7,27 @@ export default function Skill() {
 
     return (
         <div className="my-skill">
-                    <h1 className="title">{t('skillPage.title')}</h1>
-                    <ul className="skill-list">
-                        <li className="skill">
-                            <i className="fab fa-html5 skill-icon html"></i><span className="text">HTML</span>
-                        </li>
-                        <li className="skill">
-                            <i className="fab fa-css3-alt skill-icon css"></i><span className="text">CSS</span>
-                        </li>
-                        <li className="skill">
-                            <i className="fab fa-bootstrap skill-icon bootstrap"></i><span className="text">Bootstrap</span>
-                        </li>
-                        <li className="skill">
-                            <i className="fab fa-js-square skill-icon js"></i><span className="text">JavaScript</span>
-                        </li>
-                        <li className="skill">
-                            <i className="fab fa-react skill-icon react"></i><span className="text">React</span>
-                        </li>
-                        <li className="skill">
-                            <i className="fab fa-react skill-icon react-native"></i><span className="text">React Native</span>
-                        </li>
-                    </ul>
-                </div>
+            <h1 className="title">{t('skillPage.title')}</h1>
+            <ul className="skill-list">
+                <li className="skill">
+                    <i className="fab fa-html5 skill-icon html"></i><span className="text">HTML</span>
+                </li>
+                <li className="skill">
+                    <i className="fab fa-css3-alt skill-icon css"></i><span className="text">CSS</span>
+                </li>
+                <li className="skill">
+                    <i className="fab fa-bootstrap skill-icon bootstrap"></i><span className="text">Bootstrap</span>
+                </li>
+                <li className="skill">
+                    <i className="fab fa-js-square skill-icon js"></i><span className="text">JavaScript</span>
+                </li>
+                <li className="skill">
+                    <i className="fab fa-react skill-icon react"></i><span className="text">React</span>
+                </li>
+                <li className="skill">
+                    <i className="fab fa-react skill-icon react-native"></i><span className="text">React Native</span>
+                </li>
+            </ul>
+        </div>
     )
 }
